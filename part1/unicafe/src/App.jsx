abbef91ec742cb9ed21d564a0d1dcaf427cd3364
cascelland <cascelland@gmail.com>
2023-10-17 18:49:08 +0200

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,10 +11,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+    setAverage((average + 1))
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage((average + 0))
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setAverage((average - 1))
+  }
+
+  const positivePercentage = ((good * 100) / all)
+
+
 
 
   return (
@@ -27,6 +47,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average / all}</p>
+      <p>positive {positivePercentage} %</p>
     </div>
   )
 }

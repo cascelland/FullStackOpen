cf8dfffee15b6a36f92fe8fae2de0821fcdb97db
cascelland <cascelland@gmail.com>
2023-10-17 19:57:47 +0200

    1.8: unicafe step3 1.9: unicafe step4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,36 +6,57 @@ const Button = ({ handleClick, name }) => {
   )
 }
 
+const Statistics = ({ good, neutral, bad, stats }) => {
+
+  if (stats.all === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {stats.all}</p>
+      <p>average {stats.average / stats.all}</p>
+      <p>positive {(good * 100) / stats.all} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
+
+  const [stats, setStatistics] = useState({
+    all: 0,
+    average: 0,
+  })
 
   const handleGood = () => {
     setGood(good + 1)
-    setAll(all + 1)
-    setAverage((average + 1))
+    setStatistics({
+      all: stats.all + 1,
+      average: stats.average + 1,
+    })
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
-    setAll(all + 1)
-    setAverage((average + 0))
+    setStatistics({
+      ...stats,
+      all: stats.all + 1,
+    })
   }
 
   const handleBad = () => {
     setBad(bad + 1)
-    setAll(all + 1)
-    setAverage((average - 1))
+    setStatistics({
+      all: stats.all + 1,
+      average: stats.average - 1,
+    })
   }
-
-  const positivePercentage = ((good * 100) / all)
-
-
-
 
   return (
     <div>
@@ -44,12 +65,7 @@ const App = () => {
       <Button handleClick={handleNeutral} name={"neutral"} />
       <Button handleClick={handleBad} name={"bad"} />
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average / all}</p>
-      <p>positive {positivePercentage} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} stats={stats} />
     </div>
   )
 }

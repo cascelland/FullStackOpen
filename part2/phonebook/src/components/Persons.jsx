6573c2bea40d84095b29df6e557405a7457b85bf
cascelland <cascelland@gmail.com>
2023-10-22 14:30:30 +0200

    add files

--- part2/phonebook/src/components/Persons.jsx
+++
@@ -1,9 +0,0 @@
-const Persons = (props) => {
-    return (
-        <ul>
-            {props.persons.map(person => <li key={person.id}>{person.name} {person.number} <button onClick={() => props.handleDelete(person.id)}>delete</button></li>)}
-        </ul>
-    )
-}
-
-export default Persons

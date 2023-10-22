9110282c8a9e3475194cb77e901d5c5a335ac802
cascelland <cascelland@gmail.com>
2023-10-22 16:19:27 +0200

    uibuild

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
-app.use(express.serve('dist'))
+app.use(express.static('dist'))
 
 app.use(express.json())
 app.use(morgan('tiny'))

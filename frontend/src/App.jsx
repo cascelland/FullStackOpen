5c00f1808a4321e7abed2f139fdb18e580bc13c3
cascelland <cascelland@gmail.com>
2023-10-22 14:56:22 +0200

    update directories

---
+++ frontend/src/App.jsx
@@ -0,0 +1,123 @@
+import { useState, useEffect } from 'react'
+
+import Note from './components/Note'
+import Notification from './components/Notification'
+import noteService from './services/notes'
+
+/* The structural units that make up the application's 
+functional entities are React components. 
+A React component defines the HTML for structuring the 
+content, the JavaScript functions for determining 
+functionality, and also the component's styling; 
+all in one place. This is to create individual components 
+that are as independent and reusable as possible. */
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2023</em>
+    </div>
+  )
+}
+
+const App = () => {
+  const [notes, setNotes] = useState(null)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("OH NOES")
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        console.log(initialNotes)
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  if (!notes) {
+    return null
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          setErrorMessage(`the note '${note.content}' was already deleted from server`)
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000);
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App

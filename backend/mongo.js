9b906fd48485c3492abb6656abaac7e0720ff0f2
cascelland <cascelland@gmail.com>
2023-10-23 13:10:07 +0200

    uibuild

---
+++ backend/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+    console.log("Password missing")
+    process.exit(1)
+}
+
+const password = encodeURIComponent(process.argv[2])
+
+const url = `mongodb+srv://cascelland:${password}@fullstackopen.jkhuvxo.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+/* const note = new Note({
+    content: "Fourth note",
+    important: false,
+})
+
+note.save().then(result => {
+    console.log("note saved!")
+    mongoose.connection.close()
+}) */
+
+Note.find({ important: false }).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})

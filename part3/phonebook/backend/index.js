75e2776cde0562da7ae7134f83581b9bf9b1f5ae
cascelland <cascelland@gmail.com>
2023-10-23 18:00:48 +0200

    3.15: Phonebook database, step3

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -36,10 +36,9 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+    Person.findByIdAndDelete(req.params.id).then(result => {
+        res.status(204).end()
+    })
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))

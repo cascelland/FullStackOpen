1cb6ab9799cb6eb04c313130487a33e4da7b4be4
cascelland <cascelland@gmail.com>
2023-10-25 16:09:19 +0200

    4.9: Blog list tests, step2

--- part4/blogs/backend/tests/blogs_api.test.js
+++ part4/blogs/backend/tests/blogs_api.test.js
@@ -28,6 +28,14 @@ test('blogs are in the correct amount', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('blog ids are defined', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach(blog => {
+    expect(blog.id).toBeDefined()
+  })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })

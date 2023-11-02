5cedbc52e965453c7d262157c9c30d609b5a9367
cascelland <cascelland@gmail.com>
2023-11-02 14:11:16 +0100

    5.7 Blog list frontend, step7

--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [show, setShow] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+    <div style={blogStyle}>
+    {blog.title} {blog.author} <button onClick={() => setShow(!show)}>{show ? 'hide' : 'show'}</button>
+    {show && 
+      <div>
+        {blog.url}
+        <br />
+        likes: {blog.likes} <button>like</button>
+        <br />
+        {blog.user && 
+          blog.user.name
+        }
+        </div>
+    }
   </div>  
-)
+  )
+
+}
 
 export default Blog

453843d31d0e2c885b8b07d2a2daa092e0e0784e
cascelland <cascelland@gmail.com>
2023-11-09 23:46:59 +0100

    5.17: bloglist end to end testing, step1

--- part5/backend/app.js
+++ part5/backend/app.js
@@ -24,6 +24,11 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)

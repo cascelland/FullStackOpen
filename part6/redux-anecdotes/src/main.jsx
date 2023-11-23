96725665ee04aac7466c9748d245756fb3d766fe
cascelland <cascelland@gmail.com>
2023-11-23 16:14:16 +0100

    6.16 Anecdotes and the backend, step3

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -2,14 +2,9 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './store'
-import anecdoteService from './services/anecdotes'
-import { appendAnecdote } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
-anecdoteService.getAll().then(anecdotes => {
-  anecdotes.forEach(anecdote => {
-    store.dispatch(appendAnecdote(anecdote))
-  })
-})
+store.dispatch(initializeAnecdotes())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import anecdoteService from '../services/anecdotes'
 
 const initialState = []
 
@@ -26,4 +27,14 @@ const anecdoteSlice = createSlice({
 })
 
 export const {addAnecdote, addVote, appendAnecdote} = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    anecdotes.forEach(anecdote => {
+      dispatch(appendAnecdote(anecdote))
+    }); 
+  }
+}
+
 export default anecdoteSlice.reducer

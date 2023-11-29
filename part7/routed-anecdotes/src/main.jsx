4e4150a710230b465e83447708d0c176ac3716c6
cascelland <cascelland@gmail.com>
2023-11-29 20:18:06 +0100

    7.1: routed anecdotes, step1

---
+++ part7/routed-anecdotes/src/main.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>
+)
